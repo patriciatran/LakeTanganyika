020378ea40bdc45049853f928db88e2d4238bb45
Patricia Tran <ptran5@wisc.edu>
2020-03-19 18:13:10 -0500

    updating my figures

--- Figure3-RankAbundanceCurve/Bubble-Plot-2020-02-18.R
+++ Figure3-RankAbundanceCurve/Bubble-Plot-2020-02-18.R
@@ -1,5 +1,5 @@
 # Rank Abundance curve
-abundance.mags <- read.table("~/Documents/Github/LakeTanganyika/Figure3-RankAbundanceCurve/Abundance-MAGS.tsv", sep="\t", header=TRUE)
+abundance.mags <- read.table("~/Documents/Github/LakeTanganyika/Figure3-RankAbundanceCurve/Abundance-MAGS-2020-02-25.tsv", sep="\t", header=TRUE)
 
 #filter MIMAGS quals:
 library(dplyr)
@@ -15,7 +15,7 @@ library(tidyverse)
 
 abundance.mags <- abundance.mags %>% filter(MAG %in% list.Mimags)
 
-small.abundance.mags <- abundance.mags %>% select(MAG, Taxonomy, Domain, Epi, Oxy, Hypo)
+small.abundance.mags <- abundance.mags %>% select(MAG, Taxonomy, Domain)
 # Ok all good but we want simpler names
 lookup <- read.table("~/Documents/Github/LakeTanganyika/Figure3-RankAbundanceCurve/Simple_taxo_lookup.tsv", header=TRUE, sep="\t")
 lookup$Simplified.Taxonomy
@@ -27,7 +27,7 @@ small.abundance.mags %>% filter(is.na(Simplified.Taxonomy))
 
 
 ## Bubble plot:
-abundance.mags2 <- abundance.mags %>% select(-MAG,-Domain, -Epi,-Oxy,-Hypo, -Completeness, -Nb.of.Scaffolds) %>% 
+abundance.mags2 <- abundance.mags %>% select(-MAG,-Domain) %>% 
   group_by(Taxonomy)
 
 abundance.mags2 <- left_join(abundance.mags2, lookup, by="Taxonomy")
@@ -53,12 +53,12 @@ ggplot(abundance_df, aes(x = Taxonomy, y = Sample, size = NormalizedCoverage, co
 
  
 levels(abundance_df$Sample)
-abundance_df$Sample_f <- factor(abundance_df$Sample, levels(abundance_df$Sample)[c(16, 15, 14, 19, 18, 17, 20, 24, 23, 22, 21, 2, 5, 4, 3, 10, 1, 12, 7, 11, 6, 13, 9, 8)])
+#abundance_df$Sample_f <- factor(abundance_df$Sample, levels(abundance_df$Sample)[c(16, 15, 14, 19, 18, 17, 20, 24, 23, 22, 21, 2, 5, 4, 3, 10, 1, 12, 7, 11, 6, 13, 9, 8)])
 
 
-abundance_df$Sample_f
+#abundance_df$Sample_f
 
-ggplot(abundance_df, aes(x = Taxonomy, y = Sample_f, size = NormalizedCoverage, colour=NormalizedCoverage)) + 
+#ggplot(abundance_df, aes(x = Taxonomy, y = Sample_f, size = NormalizedCoverage, colour=NormalizedCoverage)) + 
   geom_point()+
   theme_bw()+
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
@@ -69,13 +69,16 @@ sample.lookup.date <- read.table("~/Documents/Github/LakeTanganyika/FigureS4-Cya
 abundance_df_info <- left_join(abundance_df,sample.lookup.date)
 
 library(lubridate)
-abundance_df_info$Date <- ymd(paste(abundance_df_info$Date_year, abundance_df_info$Date_month, abundane_df_info$Date_day, sep="-"))
+abundance_df_info$Date <- ymd(paste(abundance_df_info$Date_year, abundance_df_info$Date_month, abundance_df_info$Date_day, sep="-"))
 
-ggplot(abundance_df_info, aes(x=Taxonomy, y=Sample_f, size=NormalizedCoverage, colour=NormalizedCoverage ))+
+ggplot(abundance_df_info, aes(x=Taxonomy, y=-Depth, size=NormalizedCoverage, colour=NormalizedCoverage ))+
   geom_point()+
   facet_grid(abundance_df_info$Date, scales="free")+
   theme_bw()+
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+  theme(axis.text.x = element_text(angle = 90, hjust = 1),
+        strip.text.y = element_text(angle = 0),
+        legend.position = "top")+
+  guides(colour=guide_legend(nrow=1))
 
 # ###
 # 
@@ -178,3 +181,17 @@ ggplot(abundance_df_info, aes(x=Taxonomy, y=Sample_f, size=NormalizedCoverage, c
 #   ylab("Relative Abundance (%)")
 # 
 #   
+
+GC <- read.table("~/Documents/Github/LakeTanganyika/Figure3-RankAbundanceCurve/GC-Content-LT.csv", sep="\t", header=T)
+
+hist(GC$GC, xlim=c(0,1), n=50,
+     main="Histrogram of GC content (%) in Lake Tanganyika MAGs (n=523 MAGs)",
+     xlab="GC Content (%)",
+     ylab="Frequency")
+
+abundance.mags.GC <- left_join(abundance.mags, GC)
+
+
+# Select the 100 most abundant MAGS from 1200m to compare to the 100 most abundant MAGS in Baikal:
+top100 <- abundance.mags.GC %>% top_n(100, KigC1200)
+top100.group <-top100 %>% count(Taxonomy)
