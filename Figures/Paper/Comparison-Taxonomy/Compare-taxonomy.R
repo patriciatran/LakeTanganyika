8b067f17d741304a3073f7f97831edcd820eb5f1
Patricia Tran <ptran5@wisc.edu>
2020-11-13 13:18:11 -0600

    Comparis taxass interactive plot

--- Figures/Paper/Comparison-Taxonomy/Compare-taxonomy.R
+++ Figures/Paper/Comparison-Taxonomy/Compare-taxonomy.R
@@ -1,8 +1,15 @@
 library(readxl)
-Compare <- read_excel("Paper/Comparison-Taxonomy/Compare.xlsx")
-View(Compare)   
+Compare <- read.table("Paper/Comparison-Taxonomy/compare.rp16.gtdb.taxass.tsv", sep="\t", header=TRUE)
+#View(Compare)   
 
 library(tidyverse)
+
+
+Compare$Manual_Taxonomy <- str_replace(Compare$Manual_Taxonomy, "CP Verstraetaerchaeota", "CP Verstratearchaeota")
+Compare$Manual_Taxonomy <- str_replace(Compare$Manual_Taxonomy, "Kirimatiellacea", "Kirimatiellaeota")
+Compare$Manual_Taxonomy <- str_replace(Compare$Manual_Taxonomy, "CP Aminicemantes (OP8)", "CP Aminicenantes (CP8)")
+#Compare$Manual_Taxonomy <- str_replace(Compare$Manual_Taxonomy, "Planctomycetes \\(.*", "Phycisphaerae")
+Compare$Manual_Taxonomy <- str_replace(Compare$Manual_Taxonomy, "Verrucomicrobia.*", "Verrucomicrobia")
 
 
 Compare %>% filter(!is.na(TaxAss_combined)) %>%
@@ -14,30 +21,38 @@ Compare %>% filter(!is.na(TaxAss_combined)) %>%
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
 order.taxo <- read.table("Paper/Cazyme/order.taxonomy.tsv", header=TRUE, sep="\t")
-order.I.want <- order.taxo$Taxonomy
+order.I.want <- unique(order.taxo$Taxon.order)
 
 Compare$Manual_Taxonomy <- factor(Compare$Manual_Taxonomy, levels = order.I.want)
 
-Compare <- left_join(Compare, order.taxo, by=c("Manual_Taxonomy" = "Taxonomy"))
+order.taxo1 <- order.taxo %>% select(Taxon.order, Domain, CPR, DPANN) %>% unique()
 
-install.packages("plotly")
-library(plotly) 
+Compare <- left_join(Compare, order.taxo1, by=c("Manual_Taxonomy" = "Taxon.order"))
 
-Compare %>% filter(!is.na(TaxAss_combined)) %>%
-  group_by(Manual_Taxonomy, Match, Domain, CPR) %>%
+domain.na <- Compare %>% filter(is.na(Domain))
+#install.packages("plotly")
+#library(plotly) 
+
+Compare %>% filter(TaxAss_combined != "") %>%
+  group_by(Manual_Taxonomy, Match, Domain, CPR, DPANN) %>%
   tally() %>%
   ggplot(aes(x=Manual_Taxonomy, y=n, fill=Match))+
   geom_bar(stat = "identity")+
-  facet_grid(.~Domain+CPR, scales="free")+
+  facet_wrap(.~Domain+CPR+DPANN, scales="free")+
   theme_bw()+
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
         legend.position = "top")
-
-data <- Compare %>% filter(!is.na(TaxAss_combined)) %>%
-  group_by(Manual_Taxonomy, Match, Domain, CPR) %>%
-  tally() 
-
-fig <- plot_ly(data, x = ~Manual_Taxonomy, y = ~n, color = ~Match, type = 'bar')
-fig <- fig %>% layout(yaxis = list(title = 'Count'), barmode = 'stack', facet_row='Domain')
+ 
+Compare.plot <- Compare %>% filter(TaxAss_combined != "") %>%
+  group_by(Manual_Taxonomy, Match, Domain, CPR, DPANN) %>%
+  tally() %>%
+  ggplot(aes(x=Manual_Taxonomy, y=n, fill=Match))+
+  geom_bar(stat = "identity")+
+  facet_wrap(.~Domain+CPR+DPANN, scales="free")+
+  theme_bw()+
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
+        legend.position = "none")+
+  ylab("Number of MAGs")
+fig <- ggplotly(Compare.plot)
 
 fig
