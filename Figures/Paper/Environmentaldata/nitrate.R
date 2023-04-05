05cf671735375a8b57b35ed16eb9700f6fabbde7
Patricia Tran <ptran5@wisc.edu>
2023-04-05 08:46:37 -0500

    cleaned up code

--- Figures/Paper/Environmentaldata/nitrate.R
+++ Figures/Paper/Environmentaldata/nitrate.R
@@ -1,9 +1,9 @@
-#Nitrate:
+# Ploting the nitrate data
+# Load packages:
+
+# Load data
 nitrate <- read.table("~/Documents/Github/LakeTanganyika/Figures/Paper/Environmentaldata/Nitrate.tsv", sep="\t", header=TRUE)
-
 colnames(nitrate)
-
-library(tidyverse)
 
 ggplot(nitrate)+
   geom_point(aes(x=SRP.ug.L, y=-Depth..m.), fill="blue", alpha=0.7, size=4, pch=21, color="black")+
@@ -12,3 +12,5 @@ ggplot(nitrate)+
   ylab("Depth (m)")+
   xlab("Concentration (ug/L)")+
   theme_bw()
+
+ggsave("Figures/Paper/Environmentaldata/Nitrate.pdf", width=10, height=8)
