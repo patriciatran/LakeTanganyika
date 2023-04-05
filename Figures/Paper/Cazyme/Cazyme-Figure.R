ff58e532b1610c1cefc148bf50dad5750c8c0edb
Patricia Tran <ptran5@wisc.edu>
2023-04-05 08:36:19 -0500

    Cleaned up code in Cazyme figure

--- Figures/Paper/Cazyme/Cazyme-Figure.R
+++ Figures/Paper/Cazyme/Cazyme-Figure.R
@@ -1,33 +1,38 @@
+# Load packages:
+library(tidyverse)
+library(dplyr)
+library(reshape)
+
+
+# Load data
 dbcan <- read.delim("~/Documents/Github/LakeTanganyika/Figures/Paper/Cazyme/dbcan-2019-10-11.tsv")
 
+# Fix spelling mistakes
 dbcan$Taxonomy <- as.character(dbcan$Taxonomy)
-
-library(tidyverse)
 dbcan$Taxonomy <- str_replace(dbcan$Taxonomy, "CP Verstraetaerchaeota", "CP Verstratearchaeota")
 dbcan$Taxonomy <- str_replace(dbcan$Taxonomy, "Kirimatiellacea", "Kirimatiellaeota")
 dbcan$Taxonomy <- str_replace(dbcan$Taxonomy, "CP Aminicemantes (OP8)", "CP Aminicenantes (CP8)")
 
-
+# Convert to a numeric table
 dbcan$Cazyme.density..nb.of.CAZYME.bp.<- as.numeric(dbcan$Cazyme.density..nb.of.CAZYME.bp.)
+
 str(dbcan)
 
+# Load metabolism table:
 metabolism <- read.csv("~/Documents/Github/LakeTanganyika/Figures/Paper/Metabolism-Summary/metabolism.tsv", header=T, sep="\t")
-library(dplyr)
+
+# Only select MAGs that are MIMAG (50% complete and less than 10% contam)
 metabolism <- metabolism %>% filter(Completeness_checkm >= 50) %>% filter(Contamination_checkm <= 10)
 
+# Get a list of their names
 list.mag <- as.character(metabolism$MAG)
 
+# Subset the dbcan table using those mag names
 dbcan <- dbcan %>% filter(MAG_ID %in% list.mag)
-# boxplot(dbcan$Cazyme.density..nb.of.CAZYME.bp.~ dbcan$Taxonomy,
-#   xlab="Taxonomic group",
-#   ylab="CAZYME density per genome size (Hits/bp)")
-
-#dbcan <- dbcan[-460,]
 
 dbcan <- dbcan[,c(1:4,7:14)]
 
-#install.packages("reshape")
-library(reshape)
+
 m.dbcan <- melt(dbcan)
 #m.dbcan <- dbcan %>% gather("variable","value",Cazyme.density..nb.of.CAZYME.bp.:PL)
 
@@ -46,12 +51,6 @@ str(nb.of.MAG.each.taxa)
 
 nb.of.MAG.each.taxa$newMAGname <- paste(nb.of.MAG.each.taxa$Taxonomy," (",nb.of.MAG.each.taxa$n,")",sep="")
 
-#order.wanted <- reorder(subset.mdbcan$Taxonomy, subset.mdbcan$value, FUN= sum)
-
-#head(order.wanted)
-
-#View(order.wanted)
-
 #str(m.dbcan[which(m.dbcan$value>0),])
 
 # Only showing dbcan values greater than one
@@ -60,6 +59,7 @@ new.m.dbcan <- m.dbcan %>% left_join(nb.of.MAG.each.taxa)
 
 str(new.m.dbcan)
 
+# Get a table specifying the order of the taxonomies
 order.taxo <- read.table("Paper/Cazyme/order.taxonomy.tsv", header=TRUE, sep="\t")
 order.I.want <- order.taxo$Taxonomy
 
@@ -67,6 +67,7 @@ order.I.want
 
 new.m.dbcan$Taxonomy_f <- factor(new.m.dbcan$Taxonomy, levels=order)
 
+# Plot figures:
 bp <- ggplot(new.m.dbcan[which(new.m.dbcan$value>0),], aes(x=newMAGname, y=value)) + 
   geom_boxplot(lwd=0.4)+
   facet_grid(Domain ~ variable,scales="free",space = "free_y")+
@@ -78,9 +79,6 @@ bp <- ggplot(new.m.dbcan[which(new.m.dbcan$value>0),], aes(x=newMAGname, y=value
   ylab("Number of Hits")
 
 bp
-
-library(tidyverse)
-
 
 new.m.dbcan$Taxonomy_f2 <- factor(new.m.dbcan$Taxonomy_f, levels=order.I.want)
 
@@ -115,7 +113,7 @@ order.I.want.mag.name <- nb.of.MAG.each.taxa2$newMAGname
 new.m.dbcan$newMAGname_f <- factor(new.m.dbcan$newMAGname, levels=order.I.want.mag.name)
 
 
-# Final ploot :) !!
+# Final plot :) !!
 ggplot(new.m.dbcan[which(new.m.dbcan$value>0),], aes(x=newMAGname_f, y=value)) + 
   geom_boxplot(lwd=0.4)+
   facet_grid(Domain.y+CPR+DPANN ~ variable,scales="free",space = "free_y")+
@@ -127,63 +125,3 @@ ggplot(new.m.dbcan[which(new.m.dbcan$value>0),], aes(x=newMAGname_f, y=value)) +
   ylab("Number of Hits")
 
 ggsave("Paper/Cazyme/cazyme.2020.11.11.with.number.of.mags.2.pdf", height = 11, width = 8.5, units = "in")
-
-# 
-# ## Read data:
-# cazyme <- read.csv("~/Documents/Github/LakeTanganyika/Figures/Paper/Cazyme/cazyme.csv")
-# # Just take the GH:
-# colnames(cazyme)
-# cazyme.GH <- cazyme %>% select(MAG_ID, GH1:GH99)
-# #rownames(cazyme.GH) <- cazyme.GH$MAG_ID
-# #cazyme.GH <- cazyme.GH[,-1]
-# unique(cazyme.GH$MAG_ID)
-# 
-# # Add taxonomic info and summarize by GH
-# library(d3heatmap)
-# library(tidyverse)
-# cazyme.GH <- cazyme.GH %>% gather("GH","Value",GH1:GH99)
-# cazyme.GH <- cazyme.GH %>% filter(!is.na(Value))
-# 
-# 
-# ggplot(cazyme.GH, aes(x=MAG_ID, y=GH, fill=Value)) +
-#   geom_tile()+
-#   theme_bw()
-# 
-# # Taxonomy
-# taxo <- m.dbcan[,1:3]
-# cazyme.GH <- left_join(cazyme.GH, taxo)
-# 
-# summary <- cazyme.GH %>% group_by(SimplifiedTaxonomy, GH, Value) %>%
-#   summarise()
-# 
-# summary <- summary %>% arrange(-Value)
-# 
-# ggplot(summary, aes(x=SimplifiedTaxonomy, y=GH, fill=Value)) +
-#   geom_tile()+
-#   theme_bw()+
-#   theme(axis.text.x = element_text(angle = 90, hjust = 1))
-# 
-# 
-# taxo2 <- read.delim("~/Documents/Github/LakeTanganyika/Figures/Paper/Abundance-Plot/Simple_taxo_lookup.tsv")
-# 
-# summary2 <- left_join(summary, taxo2, by=c("SimplifiedTaxonomy"="Taxonomy"))
-# summary2$Simplified.Taxonomy <- as.character(summary2$Simplified.Taxonomy)
-# 
-# summary2$Simplified.Taxonomy2 <- ifelse(is.na(summary2$Simplified.Taxonomy), yes = summary2$SimplifiedTaxonomy, no=summary2$Simplified.Taxonomy)
-# 
-# #Remove the underscore after the subgroup:
-# summary2$GH <- str_replace(summary2$GH, "_.*","")
-# 
-# summary2 %>% group_by(Simplified.Taxonomy2, GH) %>%
-#   summarise(sum=sum(Value)) %>%
-#   ggplot(aes(x=Simplified.Taxonomy2, y=GH, fill=sum))+
-#   geom_tile()+
-#   theme_bw()+
-#   theme(axis.text.x = element_text(angle = 90, hjust = 1),
-#         text = element_text(size=8),
-#         panel.grid.major= element_line("light grey"),
-#         panel.grid.minor= element_line("black"))
-# 
-# # The plot looks ok but there are way too many rows.
-
-
