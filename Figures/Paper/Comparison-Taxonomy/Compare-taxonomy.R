64cdbf6d88885a3732c107c8352dffcca92b5a48
Patricia Tran <ptran5@wisc.edu>
2023-04-05 08:40:18 -0500

    Cleaned up code to compare taxonomies (manual, 16S, gtdb-tk)

--- Figures/Paper/Comparison-Taxonomy/Compare-taxonomy.R
+++ Figures/Paper/Comparison-Taxonomy/Compare-taxonomy.R
@@ -1,17 +1,21 @@
+# This code compares the taxonomy between manual, GTDB-tk and 16S rRNA taxonomy, when applicable.
+
+# Load libraries:
 library(readxl)
+library(tidyverse)
+
+# Load data
 Compare <- read.table("Paper/Comparison-Taxonomy/compare.rp16.gtdb.taxass.tsv", sep="\t", header=TRUE)
 #View(Compare)   
 
-library(tidyverse)
-
-
+# Fix spelling mistakes ...
 Compare$Manual_Taxonomy <- str_replace(Compare$Manual_Taxonomy, "CP Verstraetaerchaeota", "CP Verstratearchaeota")
 Compare$Manual_Taxonomy <- str_replace(Compare$Manual_Taxonomy, "Kirimatiellacea", "Kirimatiellaeota")
 Compare$Manual_Taxonomy <- str_replace(Compare$Manual_Taxonomy, "CP Aminicemantes (OP8)", "CP Aminicenantes (CP8)")
 #Compare$Manual_Taxonomy <- str_replace(Compare$Manual_Taxonomy, "Planctomycetes \\(.*", "Phycisphaerae")
 Compare$Manual_Taxonomy <- str_replace(Compare$Manual_Taxonomy, "Verrucomicrobia.*", "Verrucomicrobia")
 
-
+# Plot the 16S taxonomy:
 Compare %>% filter(!is.na(TaxAss_combined)) %>%
   group_by(Manual_Taxonomy, Match) %>%
   tally() %>%
@@ -20,6 +24,7 @@ Compare %>% filter(!is.na(TaxAss_combined)) %>%
   theme_bw()+
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
+# Reorder the taxa.
 order.taxo <- read.table("Paper/Cazyme/order.taxonomy.tsv", header=TRUE, sep="\t")
 order.I.want <- unique(order.taxo$Taxon.order)
 
